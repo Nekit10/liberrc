999f55fdc72c936dc4fe4a4835c32ac24849a15d
Nikita Serba <nikitaserba@icloud.com>
2020-02-06 19:34:56 +0200

    Added file with tests for ErrorValue math methods

---
+++ unittests/errmath_tests.cpp
@@ -0,0 +1,32 @@
+/**
+ * This file is part of liberrc.
+ *
+ *  liberrc is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of
+ *  the License, or (at your option) any later version.
+ *
+ *  liberrc is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  and GNU Lesser General Public License along with liberrc.  If not,
+ *  see <https://www.gnu.org/licenses/>.
+ */
+
+#include "gtest/gtest.h"
+
+#include "errc.h"
+
+const double ABSMAX = 0.000001;
+
+TEST(Test, Test) {
+
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
